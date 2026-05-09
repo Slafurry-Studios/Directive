dc65b7103489873b1317d2620696fe04d03a8396
muhammadzaini213 <oniichanterritory@gmail.com>
2026-05-09 15:44:37 +0800

    chore: refine folder structure

--- Assets/_Game/Scripts/Projectiles/Patterns/CircleSpawner.cs
+++ Assets/_Game/Scripts/Projectiles/Patterns/CircleSpawner.cs
@@ -4,8 +4,6 @@ public class CircleSpawner : PatternSpawner
 {
     [Header("Circle Configuration")]
     [SerializeField] private int circleCount = 12;
-    [SerializeField] private float rotationSpeed = 50f;
-
     private float currentOffsetAngle = 0f;
 
     public override void ExecutePattern(int damage, Vector2 direction, Transform position)
