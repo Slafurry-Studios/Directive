45640a28ad52fb7a7cc8c9bded13118bfc69afb8
Muhammad Zaini <163093916+muhammadzaini213@users.noreply.github.com>
2026-05-09 15:45:15 +0800

    Chore/files (#59)

--- Assets/_Game/Scripts/Characters/Player/Stats/PlayerHealth.cs
+++ Assets/_Game/Scripts/Characters/Player/Stats/PlayerHealth.cs
@@ -1,20 +1,25 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class PlayerHealth : Health
 {
     [SerializeField] private LayerMask invincibilityLayer;
     [SerializeField] private float invincibilityDuration = 1.0f;
-    
+    [SerializeField] private float blinkInterval = 0.1f;
+
     private int originalLayer;
     private Animator animator;
+    private SpriteRenderer[] spriteRenderers;
+
     protected override void Start()
     {
         base.Start();
+
         OnHealthChanged += HandlePlayerHit;
         OnDeath += HandlePlayerDeath;
+
         animator = GetComponent<Animator>();
+        spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
 
         originalLayer = gameObject.layer;
 
@@ -28,9 +33,41 @@ public class PlayerHealth : Health
         gameObject.layer = MaskToLayer(invincibilityLayer);
 
         CancelInvoke(nameof(RestoreLayer));
+        StopAllCoroutines();
+
         Invoke(nameof(RestoreLayer), invincibilityDuration);
 
+        StartCoroutine(BlinkEffect());
+
         UpdateUI(current, max);
+    }
+
+    private System.Collections.IEnumerator BlinkEffect()
+    {
+        float elapsed = 0f;
+
+        while (elapsed < invincibilityDuration)
+        {
+            SetOpacity(0.3f);
+            yield return new WaitForSeconds(blinkInterval);
+
+            SetOpacity(1f);
+            yield return new WaitForSeconds(blinkInterval);
+
+            elapsed += blinkInterval * 2f;
+        }
+
+        SetOpacity(1f);
+    }
+
+    private void SetOpacity(float alpha)
+    {
+        foreach (SpriteRenderer sr in spriteRenderers)
+        {
+            Color color = sr.color;
+            color.a = alpha;
+            sr.color = color;
+        }
     }
 
     private void UpdateUI(int current, int max)
@@ -41,7 +78,6 @@ public class PlayerHealth : Health
         }
     }
 
-
     private void HandlePlayerDeath()
     {
         Debug.Log("Player died! Show Game Over.");
@@ -51,16 +87,25 @@ public class PlayerHealth : Health
     public override void ApplyKnockback(Vector2 direction, float force)
     {
         PlayerMove move = GetComponent<PlayerMove>();
-        if (move != null) move.enabled = false;
+
+        if (move != null)
+        {
+            move.enabled = false;
+        }
 
         base.ApplyKnockback(direction, force);
+
         Invoke(nameof(EnableMovement), 0.5f);
     }
 
     private void EnableMovement()
     {
         PlayerMove move = GetComponent<PlayerMove>();
-        if (move != null) move.enabled = true;
+
+        if (move != null)
+        {
+            move.enabled = true;
+        }
     }
 
     private void RestartLevel()
@@ -77,13 +122,20 @@ public class PlayerHealth : Health
     private int MaskToLayer(LayerMask mask)
     {
         int bitmask = mask.value;
-        if (bitmask == 0) return 0;
+
+        if (bitmask == 0)
+        {
+            return 0;
+        }
+
         int result = 0;
+
         while (bitmask > 1)
         {
             bitmask >>= 1;
             result++;
         }
+
         return result;
     }
 }
